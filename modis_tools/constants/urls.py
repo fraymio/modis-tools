4176dc32d68eef8eba5d91e2fa5e5144889c9daa
Sheng Pei Wang <34722076+ShengpeiWang@users.noreply.github.com>
2022-02-28 16:10:53 -0500

    add National Snow and Ice Data Center as a datasource

--- modis_tools/constants/urls.py
+++ modis_tools/constants/urls.py
@@ -3,9 +3,10 @@ from enum import Enum
 
 
 class URLs(Enum):
-    """ URLs """
+    """URLs"""
 
     API: str = "cmr.earthdata.nasa.gov"
     URS: str = "urs.earthdata.nasa.gov"
     RESOURCE: str = "e4ftl01.cr.usgs.gov"
+    NSIDC_RESOURCE: str = "n5eil01u.ecs.nsidc.org"
     EARTHDATA: str = ".earthdata.nasa.gov"
